6097b15fd7646807c2229326db48e538f823de3f
Kiel Lofstrand <kiel.lofstrand@gmail.com>
2020-09-14 15:06:42 -0400

    SimplMD5 demo
    SimplMD5 wrapper library and demo program

---
+++ SimplCrypto/SimplCrypto/Properties/AssemblyInfo.cs
@@ -0,0 +1,8 @@
+﻿using System.Reflection;
+
+[assembly: AssemblyTitle("SimplCrypto")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("SimplCrypto")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyVersion("1.0.0.*")]
+
---
+++ SimplCrypto/SimplCrypto/SimplMD5.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Text;
+using Crestron.SimplSharp;
+using Crestron.SimplSharp.Cryptography;
+
+namespace SimplCrypto
+{
+    public class SimplMD5
+    {
+        private char[] hex_table = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
+        
+        public SimplMD5()
+        {
+        }
+
+        public string HexString(string data)
+        {
+            StringBuilder sb = new StringBuilder();
+            byte[] bytes = Encoding.ASCII.GetBytes(data);
+
+            using (var md5 = MD5.Create())
+            {
+                byte[] hash = md5.ComputeHash(bytes);
+
+                foreach (var b in hash)
+                {
+                    sb.Append(hex_table[b >> 4]);
+                    sb.Append(hex_table[b & 15]);
+                }
+            }
+
+            return sb.ToString();
+        }
+    }
+}
